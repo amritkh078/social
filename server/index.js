1fe25f83e78805925072189cc6800e5dcf3b108d
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-01 22:30:41 +0545

    re structured

--- server/index.js
+++ server/index.js
@@ -1,43 +1,46 @@
-import express from 'express' 
-import bodyParser from 'body-parser' 
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import helmet from 'helmet'
-import multer from 'multer'
-import morgan from 'morgan'
-import path from 'path'
-import { fileURLToPath } from 'node:url'
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import multer from "multer";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "node:url";
 
 // middleware
-const filename = fileURLToPath(import.meta.url); // file path 
+const filename = fileURLToPath(import.meta.url); // file path
 const dirname = path.dirname(filename); // directory path
 dotenv.config(); // load .env file
 const app = express(); // initialize express
 app.use(express.json()); // parse json
 app.use(helmet()); // secure http headers
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' })); 
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common")); // log http requests
 app.use(bodyParser.json({ limit: "30mb", extended: true })); // parse json
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // parse url encoded
 app.use(cors()); // invoke cors
-app.use("/assets", express.static(path.join(dirname, "public/assets"))); 
+app.use("/assets", express.static(path.join(dirname, "public/assets")));
 
-// file storage 
+// file storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => { // destination folder for files
-        cb(null, "public/assets");
-    },
-    filename: (req, file, cb) => { 
-        cb(null, file.originalname);
-    }
+  destination: (req, file, cb) => {
+    // destination folder for files
+    cb(null, "public/assets");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 const upload = multer({ storage }); // multer instance used to upload files
 
 // database connection
 const PORT = process.env.PORT || 6000;
-mongoose.set("strictQuery", false)
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-}).catch((error) => console.log(`${error} did not connect`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
