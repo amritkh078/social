dcd2f1650388e717220dbe74c02d2ab806ec15db
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-03 21:44:15 +0545

    new user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,9 +5,21 @@ import { User } from "../models/user.js";
 // register user
 export const register = async (req, res) => {
   try {
-    const { firstName, lastName, email, password, location, occupation } = req.body;
+    const { firstName, lastName, email, password, picturePath, friends, location, occupation } = req.body;
 
     const salt = await bcrypt.genSalt(10);
     const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 1000),
+      impressions: Math.floor(Math.random() * 1000),
+    })
   } catch (err) {}
 };
