d5bf086ff60ae94aa571dbc6ae7c441c1df0dc26
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-03 21:18:29 +0545

    auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,9 +5,9 @@ import { User } from "../models/user.js";
 // register user
 export const register = async (req, res) => {
   try {
-    const { firstName, lastName, email, password, location, occupation } =
-      req.body;
+    const { firstName, lastName, email, password, location, occupation } = req.body;
 
-      
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
   } catch (err) {}
 };
