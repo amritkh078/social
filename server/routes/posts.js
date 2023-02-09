513e9d79f2a50e4f4fb4cfbf3189039fd8dcdec4
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-09 17:17:02 +0545

    router for post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,3 +2,13 @@ import express from "express";
 import {getFeedPosts, getUserPosts, likePost} from "../controllers/posts.js";
 
 const router = express.Router();
+
+
+// read all posts
+router.get("/",verifyToken, getFeedPosts); 
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// update 
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
